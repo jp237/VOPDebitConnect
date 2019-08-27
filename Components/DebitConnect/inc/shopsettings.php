36e4a21bfa61f1333152a5c969f46077e50f7342
pheidmann <pascal.heidmann@bedarf.de>
2019-08-27 11:25:21 +0200

    Auto-Codecheck/Modernisierung via php-cs

--- Components/DebitConnect/inc/shopsettings.php
+++ Components/DebitConnect/inc/shopsettings.php
@@ -1,144 +1,160 @@
 <?php
-class shopsettings {
+/**
+ * EAP-DebitConnect (shopware Edition)
+ *
+ * V.O.P GmbH & Co. KG
+ * Hauptstraße 62
+ * 56745 Bell
+ * Telefon: +49 (2652) 529-0
+ * Telefax: +49 (2652) 529-129
+ * E-Mail: info@eaponline.de
+ * USt-IdNr.: DE 261 538 563
+ * Pers. Haft. Gesellschafter:
+ * V.O.P Verwaltungs GmbH, HRB 21231, Koblenz
+ * Geschäftsführer: Thomas Pütz
+ * Handelsregister HRA20499, Koblenz
+ */
 
-	
-	var $selectedShop;
-	var $settings;
-	var $shopsArray;
-	var $shops;
-	var $currentSetting;
-	var $currentStates;
-	var $currentPayments;
-	var $currentVorkasse;
-	var $currentSEPA;
-	var $currentHBCI;
-	var $hbciProfiles;
-	var $mahnstopCustomerGroup;
-	var $SKRSkonto;
-	var $registration;
-	var $cronjob;
-	var $hbciBlacklist;
-	var $shipping;
+class shopsettings
+{
+    public $selectedShop;
+    public $settings;
+    public $shopsArray;
+    public $shops;
+    public $currentSetting;
+    public $currentStates;
+    public $currentPayments;
+    public $currentVorkasse;
+    public $currentSEPA;
+    public $currentHBCI;
+    public $hbciProfiles;
+    public $mahnstopCustomerGroup;
+    public $SKRSkonto;
+    public $registration;
+    public $cronjob;
+    public $hbciBlacklist;
+    public $shipping;
 
-	var $hbciMailCustomerGroupDisable = null;
-	function flushsettings()
-	{
+    public $hbciMailCustomerGroupDisable = null;
 
-		$this->mahnstopCustomerGroup= null;
-		$this->currentSetting = null;
-		$this->currentStates = null;
-		$this->currentPayments = null;
-		$this->currentVorkasse = null;
-		$this->currentHBCI = null;
-		$this->SKRSkonto = null;
-		$this->settings = null;
-		$this->cronjob = null;
-		$this->shipping = null;
-		$this->hbciBlacklist = null;
-		$this->hbciMailCustomerGroupDisable = null;
-	}
-	function refresh($db,$shopList)
-	{
+    public function __construct($db)
+    {
+        //$this->refresh($db,DC()->shopList);
+    }
 
-		$this->getRegistration($db);
-		$this->getCompanySettings($db,$shopList);
-		$this->getActiveSettings();
-	}
-	
-	function getRegistration($db)
-	{
-		$this->shopsArray = $db->getSQLResults("select * from dc_firma");
-	}
-	
-	function getCompanySettings($db,$shopList)
-	{		
-	if(isset(DC()->shopList)){
-		foreach(DC()->shopList as $shopsettings)
-		{
-			if($shopsettings['settings']<1)
-			{
-				//DC()->getConf("mainsettings","{\"fristZE\":\"14\",\"zeArt\":\"2\",\"fristMA\":\"7\",\"shopwareapi\":\"\"}");
-			}
-		}
-	}
-	}
-	
-	function getHBCIProfiles(){
-		$shop = $this->selectedShop;
-		$profiles = DC()->db->getSQLResults("SELECT * from dc_hbciprofiles where shopID = ".$shop);
-		$hbciProfiles = array();
-		foreach($profiles as $profil){
-			$newProfil = new stdClass();
-			$newProfil->id = $profil['id'];
-			$newProfil->profileName = $profil['profileName'];
-			$newProfil->profileData = json_decode(DebitConnectCore::decrypt($profil['profileData']));
-			$hbciProfiles[$newProfil->id] = $newProfil;
-		}
+    public function flushsettings()
+    {
+        $this->mahnstopCustomerGroup = null;
+        $this->currentSetting = null;
+        $this->currentStates = null;
+        $this->currentPayments = null;
+        $this->currentVorkasse = null;
+        $this->currentHBCI = null;
+        $this->SKRSkonto = null;
+        $this->settings = null;
+        $this->cronjob = null;
+        $this->shipping = null;
+        $this->hbciBlacklist = null;
+        $this->hbciMailCustomerGroupDisable = null;
+    }
 
-		return $hbciProfiles;
-	}
-	
-	function updateProfile($profilId,$formData){
-		$shop = $this->selectedShop;
-		$update = new stdClass();
-		
-		$update->profileData = DebitConnectCore::encrypt(json_encode($formData));
-		DC()->db->dbUpdate("dc_hbciprofiles",$update,"id = ".(int)$profilId);
-	}
-	function getHBCIsettings($object)
-	{
-		
-		$values =  array(
-						"betreff" => $object->betreff,
-						"absender" => $object->absender,
-						"bestaetigung" => $object->bestaetigung,
-						"statusbezahlt" => $object->statusbezahlt,
-						"orderstatus" => $object->orderstatus,
-						"teilzahlung" => $object->teilzahlung,
-						"setpaymentdate" => $object->setpaymentdate,
-						"bankruecklast" => $object->bankruecklast,
-                        "zahlungsausgang" => $object->zahlungsausgang);
-						
-		//DC()->hbci->setHBCIData($values["url"],$values["blz"],$values["alias"],$values["pin"]);
-		return $values;	
-	}
-	
-	function getSKR()
-	{
-		$this->SKRSkonto["skonto"] =  json_decode(DC()->getConf("conf_skonto",""));	
-		$this->SKRSkonto["zeitraum"] =  json_decode(DC()->getConf("conf_skonto_zeitraum",""));	
-		$this->SKRSkonto["skr_payment"] =  json_decode(DC()->getConf("conf_skr_payment",""));	
-		$this->SKRSkonto["skr_buchungpos"] =  json_decode(DC()->getConf("conf_skr_buchungpos",""));	
-	}
-	function getActiveSettings()
-	{
-		//foreach($this->settings as $setting)
-	//	{
-			//if($setting['shopID'] == $this->selectedShop)
-		//	{
-				$this->registration = DC()->db->singleResult(" SELECT * from dc_firma where shopID = ".(int) $this->selectedShop);
-				$this->currentSetting = json_decode(DC()->getConf("mainsettings","{\"fristZE\":\"14\",\"zeArt\":\"2\",\"fristMA\":\"7\",\"shopwareapi\":\"\"}"));
-				$this->currentStates = json_decode(DC()->getConf("states",""));
-				$this->currentPayments = json_decode(DC()->getConf("payment",""));
-				$this->currentVorkasse = json_decode(DC()->getConf("vorkasse",""));
-				$this->currentSEPA = json_decode(DC()->getConf("sepa",""));
-				$this->shipping = json_decode(DC()->getConf('shipping','{ "overrideInvoice" : 0 , "states" : [7] }'));
-				$this->mahnstopCustomerGroup = json_decode(DC()->getConf("mahnstopCustomerGroup",""));
-				$this->cronjob = json_decode(DC()->getConf("cronjob",""));
-				$this->currentHBCI = $this->getHBCIsettings(json_decode(DC()->getConf("hbci","")));
-				$this->getSKR();
-				$this->hbciProfiles = $this->getHBCIProfiles((int) $this->selectedShop);
-				$this->hbciBlacklist = json_decode(DC()->getConf("hbciBlacklist",json_encode(array()),true));
-                $this->hbciMailCustomerGroupDisable = json_decode(DC()->getConf("hbciCustomerGroup",json_encode(array()),false));
-				//if(($this->currentPayments) == 0)$this->currentPayments[] = false;
-				//if(count($this->currentStates) == 0) $this->currentStates[] = false;
-			
-		//	}
-		//}
-	}
-	function __construct($db)
-	{
-	//$this->refresh($db,DC()->shopList);	
-	}
+    public function refresh($db, $shopList)
+    {
+        $this->getRegistration($db);
+        $this->getCompanySettings($db, $shopList);
+        $this->getActiveSettings();
+    }
+
+    public function getRegistration($db)
+    {
+        $this->shopsArray = $db->getSQLResults('select * from dc_firma');
+    }
+
+    public function getCompanySettings($db, $shopList)
+    {
+        if (isset(DC()->shopList)) {
+            foreach (DC()->shopList as $shopsettings) {
+                if ($shopsettings['settings'] < 1) {
+                    //DC()->getConf("mainsettings","{\"fristZE\":\"14\",\"zeArt\":\"2\",\"fristMA\":\"7\",\"shopwareapi\":\"\"}");
+                }
+            }
+        }
+    }
+
+    public function getHBCIProfiles()
+    {
+        $shop = $this->selectedShop;
+        $profiles = DC()->db->getSQLResults('SELECT * from dc_hbciprofiles where shopID = ' . $shop);
+        $hbciProfiles = [];
+        foreach ($profiles as $profil) {
+            $newProfil = new stdClass();
+            $newProfil->id = $profil['id'];
+            $newProfil->profileName = $profil['profileName'];
+            $newProfil->profileData = json_decode(DebitConnectCore::decrypt($profil['profileData']));
+            $hbciProfiles[$newProfil->id] = $newProfil;
+        }
+
+        return $hbciProfiles;
+    }
+
+    public function updateProfile($profilId, $formData)
+    {
+        $shop = $this->selectedShop;
+        $update = new stdClass();
+
+        $update->profileData = DebitConnectCore::encrypt(json_encode($formData));
+        DC()->db->dbUpdate('dc_hbciprofiles', $update, 'id = ' . (int) $profilId);
+    }
+
+    public function getHBCIsettings($object)
+    {
+        $values = [
+                        'betreff' => $object->betreff,
+                        'absender' => $object->absender,
+                        'bestaetigung' => $object->bestaetigung,
+                        'statusbezahlt' => $object->statusbezahlt,
+                        'orderstatus' => $object->orderstatus,
+                        'teilzahlung' => $object->teilzahlung,
+                        'setpaymentdate' => $object->setpaymentdate,
+                        'bankruecklast' => $object->bankruecklast,
+                        'zahlungsausgang' => $object->zahlungsausgang, ];
+
+        //DC()->hbci->setHBCIData($values["url"],$values["blz"],$values["alias"],$values["pin"]);
+        return $values;
+    }
+
+    public function getSKR()
+    {
+        $this->SKRSkonto['skonto'] = json_decode(DC()->getConf('conf_skonto', ''));
+        $this->SKRSkonto['zeitraum'] = json_decode(DC()->getConf('conf_skonto_zeitraum', ''));
+        $this->SKRSkonto['skr_payment'] = json_decode(DC()->getConf('conf_skr_payment', ''));
+        $this->SKRSkonto['skr_buchungpos'] = json_decode(DC()->getConf('conf_skr_buchungpos', ''));
+    }
+
+    public function getActiveSettings()
+    {
+        //foreach($this->settings as $setting)
+        //	{
+        //if($setting['shopID'] == $this->selectedShop)
+        //	{
+        $this->registration = DC()->db->singleResult(' SELECT * from dc_firma where shopID = ' . (int) $this->selectedShop);
+        $this->currentSetting = json_decode(DC()->getConf('mainsettings', '{"fristZE":"14","zeArt":"2","fristMA":"7","shopwareapi":""}'));
+        $this->currentStates = json_decode(DC()->getConf('states', ''));
+        $this->currentPayments = json_decode(DC()->getConf('payment', ''));
+        $this->currentVorkasse = json_decode(DC()->getConf('vorkasse', ''));
+        $this->currentSEPA = json_decode(DC()->getConf('sepa', ''));
+        $this->shipping = json_decode(DC()->getConf('shipping', '{ "overrideInvoice" : 0 , "states" : [7] }'));
+        $this->mahnstopCustomerGroup = json_decode(DC()->getConf('mahnstopCustomerGroup', ''));
+        $this->cronjob = json_decode(DC()->getConf('cronjob', ''));
+        $this->currentHBCI = $this->getHBCIsettings(json_decode(DC()->getConf('hbci', '')));
+        $this->getSKR();
+        $this->hbciProfiles = $this->getHBCIProfiles((int) $this->selectedShop);
+        $this->hbciBlacklist = json_decode(DC()->getConf('hbciBlacklist', json_encode([]), true));
+        $this->hbciMailCustomerGroupDisable = json_decode(DC()->getConf('hbciCustomerGroup', json_encode([]), false));
+        //if(($this->currentPayments) == 0)$this->currentPayments[] = false;
+                //if(count($this->currentStates) == 0) $this->currentStates[] = false;
+
+        //	}
+        //}
+    }
 }
-?>
--- Subscriber/Order.php
+++ Subscriber/Order.php
@@ -1,67 +1,81 @@
 <?php
-namespace VOPDebitConnect\Subscriber;
-use Enlight\Event\SubscriberInterface;
-use Symfony\Component\DependencyInjection\ContainerInterface;
+/**
+ * EAP-DebitConnect (shopware Edition)
+ *
+ * V.O.P GmbH & Co. KG
+ * Hauptstraße 62
+ * 56745 Bell
+ * Telefon: +49 (2652) 529-0
+ * Telefax: +49 (2652) 529-129
+ * E-Mail: info@eaponline.de
+ * USt-IdNr.: DE 261 538 563
+ * Pers. Haft. Gesellschafter:
+ * V.O.P Verwaltungs GmbH, HRB 21231, Koblenz
+ * Geschäftsführer: Thomas Pütz
+ * Handelsregister HRA20499, Koblenz
+ */
 
-class Order implements  SubscriberInterface
+namespace VOPDebitConnect\Subscriber;
+
+use Enlight\Event\SubscriberInterface;
+
+class Order implements SubscriberInterface
 {
-	public static function getSubscribedEvents() {
-	
-		return [
-			'Enlight_Controller_Action_PostDispatch_Backend_Order' => 'onOrderPostDispatch',
-			'Enlight_Controller_Action_PostDispatch_Backend_Customer' => 'onCustomerPostDispatch',
-		];
-	}
-	
-	 public function onOrderPostDispatch(\Enlight_Controller_ActionEventArgs $args)
-	{
-	try
-	{
-	
-		/** @var \Enlight_Controller_Action $controller */
-		$controller = $args->getSubject();
-		
-		$view = $controller->View();
-		$request = $controller->Request();
-		$response = $args->getResponse();
-		$orderId = $args->getRequest()->getParam('orderID');
-		if($orderId>0)	$view->assign("pkOrder",$orderId);
-		$view->addTemplateDir(__DIR__ . '/../Views/');
-		if ($request->getActionName() == 'index') {
-			$view->extendsTemplate('backend/v_o_p_debit_connect/orderapp.js');
-		}
-	
-		if ($request->getActionName() == 'load') {
-			$view->extendsTemplate('backend/v_o_p_debit_connect/view/detail/orderwindow.js');
-		}
-	}catch(Exception $e){
-	
-	}
-	}
-	 public function onCustomerPostDispatch(\Enlight_Controller_ActionEventArgs $args)
-	{
-	try
-	{
-	
-		/** @var \Enlight_Controller_Action $controller */
-		$controller = $args->getSubject();
-		
-		$view = $controller->View();
-		$request = $controller->Request();
-		$response = $args->getResponse();
-		$customerId = $args->getRequest()->getParam('customerID');
-		if($customerId>0)	$view->assign("pkCustomer",$customerId);
-		$view->addTemplateDir(__DIR__ . '/../Views/');
-		if ($request->getActionName() == 'index') {
-			$view->extendsTemplate('backend/v_o_p_debit_connect/customerapp.js');
-		}
-	
-		if ($request->getActionName() == 'load') {
-			$view->extendsTemplate('backend/v_o_p_debit_connect/view/detail/customerwindow.js');
-		}
-	}catch(Exception $e){
-	
-	}
-	}
-	
+    public static function getSubscribedEvents()
+    {
+        return [
+            'Enlight_Controller_Action_PostDispatch_Backend_Order' => 'onOrderPostDispatch',
+            'Enlight_Controller_Action_PostDispatch_Backend_Customer' => 'onCustomerPostDispatch',
+        ];
+    }
+
+    public function onOrderPostDispatch(\Enlight_Controller_ActionEventArgs $args)
+    {
+        try {
+            /** @var \Enlight_Controller_Action $controller */
+            $controller = $args->getSubject();
+
+            $view = $controller->View();
+            $request = $controller->Request();
+            $response = $args->getResponse();
+            $orderId = $args->getRequest()->getParam('orderID');
+            if ($orderId > 0) {
+                $view->assign('pkOrder', $orderId);
+            }
+            $view->addTemplateDir(__DIR__ . '/../Views/');
+            if ($request->getActionName() == 'index') {
+                $view->extendsTemplate('backend/v_o_p_debit_connect/orderapp.js');
+            }
+
+            if ($request->getActionName() == 'load') {
+                $view->extendsTemplate('backend/v_o_p_debit_connect/view/detail/orderwindow.js');
+            }
+        } catch (Exception $e) {
+        }
+    }
+
+    public function onCustomerPostDispatch(\Enlight_Controller_ActionEventArgs $args)
+    {
+        try {
+            /** @var \Enlight_Controller_Action $controller */
+            $controller = $args->getSubject();
+
+            $view = $controller->View();
+            $request = $controller->Request();
+            $response = $args->getResponse();
+            $customerId = $args->getRequest()->getParam('customerID');
+            if ($customerId > 0) {
+                $view->assign('pkCustomer', $customerId);
+            }
+            $view->addTemplateDir(__DIR__ . '/../Views/');
+            if ($request->getActionName() == 'index') {
+                $view->extendsTemplate('backend/v_o_p_debit_connect/customerapp.js');
+            }
+
+            if ($request->getActionName() == 'load') {
+                $view->extendsTemplate('backend/v_o_p_debit_connect/view/detail/customerwindow.js');
+            }
+        } catch (Exception $e) {
+        }
+    }
 }
