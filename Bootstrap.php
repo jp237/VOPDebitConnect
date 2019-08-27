079fd17f5887f7390c5e98aea38fb2eb7c417ebe
pheidmann <pascal.heidmann@bedarf.de>
2019-08-27 11:22:52 +0200

    moved composer includes into vendor folder

--- Bootstrap.php
+++ Bootstrap.php
@@ -1,37 +1,23 @@
 <?php
-/**
- * EAP-DebitConnect (shopware Edition)
- *
- * V.O.P GmbH & Co. KG
- * Hauptstraße 62
- * 56745 Bell
- * Telefon: +49 (2652) 529-0
- * Telefax: +49 (2652) 529-129
- * E-Mail: info@eaponline.de
- * USt-IdNr.: DE 261 538 563
- * Pers. Haft. Gesellschafter:
- * V.O.P Verwaltungs GmbH, HRB 21231, Koblenz
- * Geschäftsführer: Thomas Pütz
- * Handelsregister HRA20499, Koblenz
- */
-
-use VOPDebitConnect\Subscriber\Backend;
-use VOPDebitConnect\Subscriber\Cronjob;
-use VOPDebitConnect\Subscriber\Frontend;
-use VOPDebitConnect\Subscriber\Order;
-
-require_once __DIR__ . '/vendor/autoload.php';
+require_once __DIR__. '/vendor/autoload.php';
 require_once __DIR__ . '/Components/CSRFWhitelistAware.php';
 require_once __DIR__ . '/Components/Services/Cronjob.php';
-require_once __DIR__ . '/Subscriber/Backend.php';
-require_once __DIR__ . '/Subscriber/Frontend.php';
-require_once __DIR__ . '/Subscriber/Order.php';
-require_once __DIR__ . '/Subscriber/Cronjob.php';
+require_once __DIR__.'/Subscriber/Backend.php';
+require_once __DIR__.'/Subscriber/Frontend.php';
+require_once __DIR__.'/Subscriber/Order.php';
+require_once __DIR__.'/Subscriber/Cronjob.php';
 
+/*
+ * (c) shopware AG <info@shopware.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 class Shopware_Plugins_Backend_VOPDebitConnect_Bootstrap extends Shopware_Components_Plugin_Bootstrap
 {
-    const TEMPLATE_PATH = 'Views/';
-
+	
+	const TEMPLATE_PATH = 'Views/';
+	
     /**
      * Returns an array with the capabilities of the plugin.
      *
@@ -42,36 +28,57 @@ class Shopware_Plugins_Backend_VOPDebitConnect_Bootstrap extends Shopware_Compon
         return [
             'install' => true,
             'enable' => true,
-            'update' => true,
+            'update' => true
         ];
     }
 
-    //Internals STOP
+ 
+    /**
+     * After init event of the bootstrap class.
+     *
+     * The afterInit function registers the custom plugin models.
+     */
+	 
+	 private function GetPluginInformationValue($sValueName)
+	{
+		$pluginJSON = json_decode(file_get_contents($this->Path() . 'plugin.json'), true);
 
-    //Informations START
-    public function getVersion()
+        if ($pluginJSON) {
+            return $pluginJSON[$sValueName];
+        } else {
+            throw new Exception('The plugin has an invalid plugin.json file.');
+        }
+	}
+	//Internals STOP
+	
+	//Informations START	
+	/*
+	/**
+ * checkLicense()-method for VOPDebitConnect
+ */
+
+	 public function getVersion()
     {
-        return $this->GetPluginInformationValue('currentVersion');
+		return $this->GetPluginInformationValue('currentVersion');
     }
-
-    public function getLabel()
+	
+	public function getLabel()
     {
         return $this->GetPluginInformationValue('label')['de'];
     }
-
-    public function getInfo()
+	
+	public function getInfo()
     {
-        return [
+		return array(
             'label' => $this->getLabel(),
             'version' => $this->getVersion(),
-            'copyright' => $this->GetPluginInformationValue('copyright'),
-            'author' => $this->GetPluginInformationValue('author'),
+			'copyright' => $this->GetPluginInformationValue('copyright'),
+			'author' => $this->GetPluginInformationValue('author'),
             'link' => $this->GetPluginInformationValue('link'),
-            'support' => $this->GetPluginInformationValue('support'),
-            'description' => $this->GetPluginInformationValue('description'),
-        ];
+			'support' => $this->GetPluginInformationValue('support'),
+            'description' => $this->GetPluginInformationValue('description')
+        );
     }
-
     public function afterInit()
     {
         $this->registerCustomModels();
@@ -84,93 +91,113 @@ class Shopware_Plugins_Backend_VOPDebitConnect_Bootstrap extends Shopware_Compon
      *
      * @return bool
      */
-    public function update($version)
-    {
-        $install = $this->runInstallUpdate($version);
-        if ($install === true) {
-            return true;
-        }
-
-        return ['success' => false, 'message' => $install];
+	 
+	 public function update($version)
+	 {
+		$install = $this->runInstallUpdate($version);
+		if($install===true)
+		{
+		 return true;
+		}
+		else
+		{
+			 return ['success' => false, 'message' => $install];
+		}
+	 }
+	 
+/*	  public function afterInit() {
+      $this->Application()->Loader()->registerNamespace('Shopware\BoniGateway', $this->Path());
     }
+*/
 
-    /*	  public function afterInit() {
-          $this->Application()->Loader()->registerNamespace('Shopware\BoniGateway', $this->Path());
-        }
-    */
+public function installRemoveCronJobs($cronjob,$install = true){
+	if($install){
+	  Shopware()->Db()->insert(
+            's_crontab',
+            [
+                'name'             =>	$cronjob['name'],
+                'action'           =>   $cronjob['action'],
+                'next'             => date("Y-m-d H:i:s"),
+                'start'            => date("Y-m-d H:i:s"),
+                'interval'       => $cronjob['interval'],
+                'active'           => 0,
+                'pluginID'         => null
+            ]
+        );
+	}else{
+		Shopware()->Db()->query("delete from s_crontab where `name` = '".$cronjob['name']."' and action = '".$cronjob['action']."'");
+	}
+}
+		
+		public function onStartDispatch(Enlight_Event_EventArgs $args) {
+			
+			$this->Application()->Loader()->registerNamespace('\VOPDebitConnect',$this->Path());
+			
+			/** @var Shopware\Components\DependencyInjection\Container $container */
+			$container = Shopware()->Container();
+			
+			
+			$subscribers = [
+				new \VOPDebitConnect\Subscriber\Backend(),
+				new \VOPDebitConnect\Subscriber\Frontend(),
+				new \VOPDebitConnect\Subscriber\Order(),
+				new \VOPDebitConnect\Subscriber\Cronjob()
+			];
+			foreach ($subscribers as $subscriber) {
+				$this->Application()->Events()->addSubscriber($subscriber);
+			}
 
-    public function installRemoveCronJobs($cronjob, $install = true)
-    {
-        if ($install) {
-            Shopware()->Db()->insert(
-                's_crontab',
-                [
-                    'name' => $cronjob['name'],
-                    'action' => $cronjob['action'],
-                    'next' => date('Y-m-d H:i:s'),
-                    'start' => date('Y-m-d H:i:s'),
-                    'interval' => $cronjob['interval'],
-                    'active' => 0,
-                    'pluginID' => null,
-                ]
-            );
-        } else {
-            Shopware()->Db()->query("delete from s_crontab where `name` = '" . $cronjob['name'] . "' and action = '" . $cronjob['action'] . "'");
-        }
-    }
+		
+		}
 
-    public function onStartDispatch(Enlight_Event_EventArgs $args)
-    {
-        $this->Application()->Loader()->registerNamespace('\VOPDebitConnect', $this->Path());
+	 
+	
+	
+	 public function runInstallUpdate($version = "0.0.0")
+	 {
 
-        /** @var Shopware\Components\DependencyInjection\Container $container */
-        $container = Shopware()->Container();
-
-        $subscribers = [
-            new Backend(),
-            new Frontend(),
-            new Order(),
-            new Cronjob(),
-        ];
-        foreach ($subscribers as $subscriber) {
-            $this->Application()->Events()->addSubscriber($subscriber);
-        }
-    }
-
-    public function runInstallUpdate($version = '0.0.0')
-    {
-        $intVersion = (int) str_replace('.', '', $version);
-
-        if (version_compare($version, '0.0.1', '<')) {
-            $this->subscribeEvent(
-                'Enlight_Controller_Front_DispatchLoopStartup',
-                'onStartDispatch'
-            );
-            $this->registerController('Backend', 'VOPDebitConnect');
-            $this->createMenu();
-        }
-
-        if (version_compare($version, '0.2.4', '<')) {
-            $this->installRemoveCronJobs(['name' => 'DebitConnect',
-                'action' => 'Shopware_CronJob_VOPCronjob',
-                'interval' => 600, ]);
-        }
-
-        return true;
-    }
-
+		 $intVersion = (int) str_replace(".","",$version);
+		 
+		 if(version_compare($version ,"0.0.1" , "<"))
+		 {
+			 $this->subscribeEvent(
+			'Enlight_Controller_Front_DispatchLoopStartup',
+			'onStartDispatch'
+		);
+			 $this->registerController('Backend', 'VOPDebitConnect');
+			 $this->createMenu();
+		 }
+		  
+		  	 if(version_compare($version ,"0.2.4" , "<"))
+		 {
+			 
+		
+				
+      		$this->installRemoveCronJobs(array("name" => "DebitConnect",
+											"action" => "Shopware_CronJob_VOPCronjob",
+											"interval" => 600));
+			 
+		 }
+		 
+		 return true;
+	 }
+	 
+	 
     public function install()
     {
         try {
             $install = $this->runInstallUpdate();
-            if ($install === true) {
-                return [
-                    'success' => true,
-                    'invalidateCache' => ['backend'],
-                ];
-            }
-
-            return ['success' => false, 'message' => $install];
+          if( $install===true)
+		  {
+	
+            return [
+                'success' => true,
+                'invalidateCache' => ['backend']
+            ];
+		  }else
+		  {
+			   return ['success' => false, 'message' => $install];
+		  }
         } catch (Exception $e) {
             return ['success' => false, 'message' => $e->getMessage()];
         }
@@ -183,11 +210,11 @@ class Shopware_Plugins_Backend_VOPDebitConnect_Bootstrap extends Shopware_Compon
      */
     public function createMenu()
     {
-        $this->createMenuItem([
-            'label' => 'DebitConnect',
-            'active' => 1,
-            'onclick' => 'Shopware.ModuleManager.createSimplifiedModule("VOPDebitConnect", { "title": "DebitConnect" })',
-        ]);
+	$this->createMenuItem(array(
+			'label' => 'DebitConnect',
+			'active' => 1,
+			'onclick' => 'Shopware.ModuleManager.createSimplifiedModule("VOPDebitConnect", { "title": "DebitConnect" })'
+		));
     }
 
     /**
@@ -199,31 +226,22 @@ class Shopware_Plugins_Backend_VOPDebitConnect_Bootstrap extends Shopware_Compon
     public function uninstall()
     {
         Shopware()->BackendSession()->DebitConnect = null;
-        // REMOVE CRONJOBS
-        $this->installRemoveCronJobs(['name' => 'DebitConnect',
-            'action' => 'Shopware_CronJob_VOPCronjob',
-            'interval' => 3600, ], false);
-
+		// REMOVE CRONJOBS
+		$this->installRemoveCronJobs(array("name" => "DebitConnect",
+											"action" => "Shopware_CronJob_VOPCronjob",
+											"interval" => 3600),false);
+		
+											
         return true;
     }
+	
 
-    public function DebitConnectZahlungsabgleich(Shopware_Components_Cron_CronJob $job)
-    {
-        return __DIR__ . '/Controller/VOPCronjob.php';
+	 public function DebitConnectZahlungsabgleich(\Shopware_Components_Cron_CronJob $job)
+    {	
+		return dirname(__FILE__).'/Controller/VOPCronjob.php';
     }
-
-    /**
-     * After init event of the bootstrap class.
-     *
-     * The afterInit function registers the custom plugin models.
-     */
-    private function GetPluginInformationValue($sValueName)
-    {
-        $pluginJSON = json_decode(file_get_contents($this->Path() . 'plugin.json'), true);
-
-        if ($pluginJSON) {
-            return $pluginJSON[$sValueName];
-        }
-        throw new Exception('The plugin has an invalid plugin.json file.');
-    }
+	
+	
+   
+	
 }
